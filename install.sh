1047b707530902f9802a88d453b005232b420ffe
Amantaev77 <75737050+Amantaev77@users.noreply.github.com>
2024-04-09 14:11:52 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Проверьте запущен скрипт от имени Root
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
-  exit 1
-fi
-
-username=$(id -u -n 1000)
-builddir=$(pwd)
-
 # Обновление пакетов и системы
 apt update -y
 apt upgrade -y
@@ -30,4 +21,4 @@ nala install yandex-browser-stable
 
 
 # Use nala
-bash scripts/usenala
+#bash scripts/usenala
